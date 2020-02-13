03e986106b86653c14d2d9dcf5a8e7b7c0474b5c
Kris Nye <knye@adobe.com>
2020-02-13 09:50:01 -0800

    added license header

--- watch.sh
+++ watch.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Copyright 2019 Adobe
+# All Rights Reserved.
+
+# NOTICE: Adobe permits you to use, modify, and distribute this file in
+# accordance with the terms of the Adobe license agreement accompanying
+# it. If you have received this file from a source other than Adobe,
+# then your use, modification, or distribution of it requires the prior
+# written permission of Adobe. 
+
 # this is the development watch script for those with access
 # to the torq-native uxp runtime.
 # it is assumed that it's installed to a peer directory.
