5d68557c7b3ac711d113436b2d43c27c4eb291d6
Kris Nye <knye@adobe.com>
2020-03-31 05:49:02 -0700

    adding webpack helper and rs3 alpha components

--- src/webpack/index.ts
+++ src/webpack/index.ts
@@ -2,6 +2,17 @@ import { buildConfig, uxp } from "@torq-native/webpack-builder";
 import fs from "fs";
 import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+// import ResolverPlugin from "./ResolverPlugin";
+
+//  we have to specify loaders by string,
+//  BUT we don't want to force clients to import all the loaders
+//  so by using require.resolve we allow them to use our packages loaders.
+const sourceMapLoader = require.resolve("source-map-loader");
+const styleLoader = require.resolve("style-loader");
+const cssLoader = require.resolve("css-loader");
+const sassLoader = require.resolve("sass-loader");
+const tsLoader = require.resolve("ts-loader");
+const fileLoader = require.resolve("file-loader");
 
 type Options = {
     /**
@@ -9,18 +20,19 @@ type Options = {
      * Must contain a package.json file.
      */
     project: string,
+    platform: "web" | "uxp",
     production: any,
     version: any,
 };
 
 export function helper(options: Options, config?: any) {
-    const { project: root, production, version } = options;
+    const { project: root, production, platform, version } = options;
     const packageJson = JSON.parse(fs.readFileSync(path.join(root, "package.json"), "utf8"));
     const SRC = path.join(root, "src");
-    const DEST = path.join(root, "dist");
+    const DEST = path.join(root, "dist", platform);
     const fullVersion = version || packageJson.version;
 
-    return buildConfig({
+    const result = buildConfig({
         production,
 
         context: root,
@@ -40,13 +52,12 @@ export function helper(options: Options, config?: any) {
 
         libraries: [
             // require('./webpack.helper.js'),
-            require('@torq-native/react-spectrum/webpack.helper.js'),
+            // require('@torq-native/react-spectrum/webpack.helper.js'),
         ],
 
         config: {
             plugins: [
                 new HtmlWebpackPlugin({
-                    title: 'uxp-starter-pack',
                     appVersionNumber: fullVersion,
                     template: path.join(SRC, 'index.html'),
                 }),
@@ -63,46 +74,47 @@ export function helper(options: Options, config?: any) {
             ],
             resolve: {
                 // Add '.ts' and '.tsx' as resolvable extensions.
-                extensions: [".ts", ".tsx", ".js", "/index.js"]
+                extensions: [".ts", ".tsx", ".js", "/index.js"],
+                // plugins: platform === "uxp" ? [new ResolverPlugin()] : []
             },
             module: {
                 rules: [
                     {
                         test: /\.ts(x?)$/,
                         exclude: /node_modules/,
-                        use: [
-                            {
-                                loader: "ts-loader"
-                            }
-                        ]
+                        use: [ tsLoader ]
                     },
                     // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
                     {
                         enforce: "pre",
                         test: /\.js$/,
-                        loader: "source-map-loader"
+                        loader: sourceMapLoader
                     },
                     {
                         test:/\.css$/,
-                        use:['style-loader','css-loader']
+                        use:[ styleLoader, cssLoader ]
                     },
                     {
                         test: /\.s(a|c)ss$/,
                         exclude: /\.module.(s(a|c)ss)$/,
-                        loader: [
-                          'style-loader',
-                          'css-loader',
-                          'sass-loader'
-                        ]
+                        loader: [ styleLoader, cssLoader, sassLoader ]
                     },
                     {
                         test: /\.(png|svg|jpg|gif)$/,
-                        use: [
-                            'file-loader'
-                        ]
+                        use: [ fileLoader ]
                     }            
                 ]
             }
         },
     });
+
+    // we are going to pre-empt the module resolution directories provided by buildConfig
+    //  we want our /lib folder to be searched before the clients node_modules
+    //  this way it will find
+    //  /lib/@react-spectrum/button before node_modules/@react-spectrum/button
+    if (platform === "uxp") {
+        result.resolve.modules.unshift(path.join(__dirname, "../../lib"));
+    }
+
+    return result;
 }
